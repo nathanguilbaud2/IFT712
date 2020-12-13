c93b6799152c03e17e44a07b237d219870f1da1e
nathanguilbaud2 <nathan.guilbaud@edu.esiee.fr>
2020-12-13 21:43:09 +0100

    Modification de Projet.py pour entrainer en changenat les parametres des algorithmes

--- Projet.py
+++ Projet.py
@@ -13,7 +13,6 @@ from sklearn import svm
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.ensemble import BaggingClassifier
-
 def main(): 
     if len(sys.argv) < 2:
         usage = "\n Usage: python All_2.py choix_algorithme recherche_parametres generer_soumission\
@@ -41,6 +40,9 @@ def main():
     # Nous allons étudier 8 algorithmes différents
     liste_variables=[[[0] for j in range(len(x_entrainement))] for j in range(7)]    
     liste_variables_test=[[[0] for j in range(len(x_test))] for j in range(7)]
+    
+    # Liste des parametres pour chaque algorithmes
+    liste_parametres_algorithme=[ [1,1] , [100,None] , [100,1] , [6,1] , [1,"linear"] , [1,"ball_tree"] , ["svd",1] , [100,10] ]
     
     liste_variables_label=["Utilisation de Margin seulement","Utilisation de Shape seulement","Utilisation de Texture seulement","Utilisation de Margin et Shape","Utilisation de Margin et Texture","Utilisation de Shape et Texture","Utilisation de toutes les variables"]
     list_Algorithme_label=["~~~~~~~~~~~~~ GradientBoostingClassifier ~~~~~~~~~~~~~~","~~~~~~~~~~~~~~~~~~~~ Random-Forest ~~~~~~~~~~~~~~~~~~~~","~~~~~~~~~~~~~~~~~~~~~~ ADA-Boost ~~~~~~~~~~~~~~~~~~~~~~","~~~~~~~~~~~~~~~~~~~~~ Decision-Tree ~~~~~~~~~~~~~~~~~~~~~","~~~~~~~~~~~~~~~~~ SVM ~~~~~~~~~~~~~~~~~~","~~~~~~~~~~~~~~~~~~~~~ K-nearest neighbors ~~~~~~~~~~~~~~~~~~~~~","~~~~~~~~~~~~~~~~~~ Linear Discriminant Analysis ~~~~~~~~~~~~~~~~~~","~~~~~~~~~~~~~~~~~~ Bagging Classifier ~~~~~~~~~~~~~~~~~~"]
@@ -71,43 +73,43 @@ def main():
         for algorithme_choisi in range(len(list_Algorithme_label)):
             print("\n",list_Algorithme_label[algorithme_choisi])
             for variable_choisie in range(len(liste_variables)):
-                Run_algorithme(liste_variables_label[variable_choisie],liste_variables[variable_choisie],t_entrainement,algorithme_choisi)
+                Run_algorithme(liste_variables_label[variable_choisie], liste_variables[variable_choisie], t_entrainement, liste_parametres_algorithme[algorithme_choisi], algorithme_choisi)
 
     else: # Pour un algorithme
         print("\n",list_Algorithme_label[choix_algorithme])
         
         for variable_choisie in range(len(liste_variables)):
-            Run_algorithme(liste_variables_label[variable_choisie],liste_variables[variable_choisie],t_entrainement,choix_algorithme)
+            Run_algorithme(liste_variables_label[variable_choisie], liste_variables[variable_choisie], t_entrainement, liste_parametres_algorithme[choix_algorithme], choix_algorithme)
     
 """
 Fonction qui entraine un modele en fontion de l'algorithme choisi
 """ 
-def Run_algorithme(label, x_entrainement, t_entrainement, numero_algorithme):
+def Run_algorithme(label, x_entrainement, t_entrainement, liste_parametres_algorithme ,numero_algorithme):
     print(label)
     # Choix de l'algorithme a utiliser
     if(numero_algorithme==0): # 0 = Gradient Boosting Classifier
-        clf = GradientBoostingClassifier()
+        clf = GradientBoostingClassifier(n_estimators=liste_parametres_algorithme[0], learning_rate=liste_parametres_algorithme[1], random_state=0)
 
     elif(numero_algorithme==1): # 1 = Random Forest Classifier
-        clf = RandomForestClassifier()
+        clf = RandomForestClassifier(n_estimators=liste_parametres_algorithme[0], max_depth=liste_parametres_algorithme[1], random_state=0)
 
     elif(numero_algorithme==2): # 2 = ADA-Boost Classifier
-        clf = AdaBoostClassifier()
+        clf = AdaBoostClassifier(n_estimators=liste_parametres_algorithme[0], learning_rate=liste_parametres_algorithme[1], random_state=0)
 
     elif(numero_algorithme==3): # 3 = Decision Tree Classifier
-        clf = tree.DecisionTreeClassifier()
+        clf = tree.DecisionTreeClassifier(min_samples_split=liste_parametres_algorithme[0], max_depth=liste_parametres_algorithme[1], random_state=0)
 
     elif(numero_algorithme==4): # 4 = SVM
-        clf = svm.SVC()
+        clf = svm.SVC(C=liste_parametres_algorithme[0], kernel=liste_parametres_algorithme[1], probability=True)
 
     elif(numero_algorithme==5): # 5 = K-Nearest Neighbors
-        clf = KNeighborsClassifier()
+        clf = KNeighborsClassifier(n_neighbors=liste_parametres_algorithme[0], algorithm=liste_parametres_algorithme[1])
 
     elif(numero_algorithme==6): # 6 = Linear Discriminant Analysis
-        clf = LinearDiscriminantAnalysis()
+        clf = LinearDiscriminantAnalysis(solver=liste_parametres_algorithme[0], tol=liste_parametres_algorithme[1])
 
     elif(numero_algorithme==7): # 7 = Quadratic Discriminant Analysis
-        clf = BaggingClassifier()
+        clf = BaggingClassifier(n_estimators=liste_parametres_algorithme[0], max_samples=liste_parametres_algorithme[1])
 
     # Validation croisee #
     precision_entrainement=0
@@ -131,7 +133,8 @@ def Run_algorithme(label, x_entrainement, t_entrainement, numero_algorithme):
     print("Précision données de test : " , precision_test, " %\n")
     
     #return precision_test # On retourne la moyenne de la precision des donnees de test sur les 10 entrainements
-    
+ 
+   
 
 if __name__ == "__main__":
     main()
